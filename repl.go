ed60bc209caa71c184553ba82eccd37fd8332624
Nevins Bartolomeo <nevins.bartolomeo@aocsolutions.com>
2015-05-04 15:30:28 -0400

    adding additional fields

--- repl.go
+++ repl.go
@@ -6,15 +6,23 @@ import (
 
 // for ReplSet Config files
 
-type Host struct {
-	ID   int    `bson:"_id"`
-	Host string `bson:"host"`
+type RsConf struct {
+	ID       string                 `bson:"_id"`
+	Version  int64                  `bson:"version"`
+	Members  []*Host                `bson:"members"`
+	Settings map[string]interface{} `bson:"settings"`
 }
 
-type RsConf struct {
-	ID      int    `bson:"_id"`
-	Version int    `bson:"version"`
-	Members []Host `bson:"members"`
+type Host struct {
+	ID           int64             `bson:"_id"`
+	Host         string            `bson:"host"`
+	ArbiterOnly  bool              `bson:"arbiterOnly"`
+	BuildIndexes bool              `bson:"buildIndexes"`
+	Hidden       bool              `bson:"hidden"`
+	Priority     int64             `bson:"priority"`
+	Tags         map[string]string `bson:"tags"`
+	SlaveDelay   int64             `bson:"slaveDelay"`
+	Votes        int64             `bson:"votes"`
 }
 
 // rs.status()
